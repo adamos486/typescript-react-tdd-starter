5c910a7a81c6d8334d0464c2f35188d2d2d41091
Adam Cobb <adamos486@gmail.com>
2019-05-23 14:24:38 -0700

    Fixing more things and adding some flava

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,22 @@
 import * as React from "react";
+import styled from 'styled-components';
 
 export interface Props {}
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 38px;
+  font-family: "Comic Sans MS", cursive, sans-serif;
+  align-items: center;
+`;
+
 const App: React.FC = (props: Props) => {
   return (
-    <div />
+    <Container>
+      Hello, there!
+      <img src="https://i.imgur.com/NA9Cn1Q.jpg" />
+    </Container>
   );
 };
 
